3986557581aa07050b40e1b4c1a701acc1274fbc
Manya <mayavi1319@gmail.com>
2025-02-13 10:53:33 +0530

    Testing API #2

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const data = require('./data.json');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.post('/books', (req, res) => {
+  const newBook = req.body;
+  data.push(newBook);
+  res.json(newBook);
+});
+
+app.get('/books', (req, res) => {
+  res.json(data);
+});
+
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id;
+  const book = data.find((book) => book.book_id === id);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).send("Bad Request");
+  }
+});
+
+app.put('/books/:id', (req, res) => {
+  const id = req.params.id;
+  const bookIndex = data.findIndex((book) => book.id === id);
+  if (bookIndex !== -1) {
+    data[bookIndex] = req.body;
+    res.json(data[bookIndex]);
+  } else {
+    res.status(404).send("Bad Request");
+  }
+});
+
+app.delete('/books/:id', (req, res) => {
+  const id = req.params.id;
+  const bookIndex = data.findIndex((book) => book.id === id);
+  if (bookIndex !== -1) {
+    data.splice(bookIndex, 1);
+    res.json(data);
+  } else {
+    res.status(404).send("Bad Request");
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
